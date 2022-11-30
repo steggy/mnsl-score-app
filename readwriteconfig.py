428e84c05a368a9b02c0d2fcc7c0377f020ce9b0
Steggy <steggy@gmail.com>
2022-11-29 18:04:30 -0800

    Add config read/write class

---
+++ readwriteconfig.py
@@ -0,0 +1,20 @@
+from configparser import ConfigParser
+
+class Config():
+    def __init__(self,cfile):
+        self.cfile = cfile
+
+    def Fetch(self,sect):
+        config = ConfigParser()
+        config.read(self.cfile)
+        out = [tup[1] for tup in list(config.items(sect))]
+        return out
+
+    def Update(self,sect,keyvaluedict):
+        config = ConfigParser()
+        config.read(self.cfile)
+        for x, y in keyvaluedict.items():
+            config.set(sect,x,y)
+        with open(self.cfile,'w') as configfile:
+            config.write(configfile)
+
